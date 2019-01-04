91db14f41353423a749f04915f9d409fe29b0c7f
koliver-purdye <kristien.oliver-purdye@students.plymouth.ac.uk>
2019-01-04 22:06:14 +0000

    Seventh Commit, 'Navigation Between Pages now work whilst hosted on an HTTP-SERVER' It needs to work on NODE.JS which is yet to be done. Controllers need to be re-evaluated still. Database connection successful

--- APP/server.js
+++ APP/server.js
@@ -1,8 +1,29 @@
-var express = require('express');
-var app = express();
-app.use(express.static("APP")); // App will be the same folder name.
-app.get('/', function (req, res) {
-res.redirect('/');
-});
-app.listen(8080, 'localhost');
-console.log("MyProject Server is Listening on port 8080");
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+
+
+app.listen(port, () => console.log("Example app listening on port ${port}!"))
+
+var MongoClient = require('mongodb').MongoClient
+var MongoDB = require('mongodb')
+
+MongoClient.connect('mongodb://localhost:27017',function (err, client)
+{
+    if (err) throw (err)
+
+    console.log("DB Connection Successful")
+    var db = client.db('Users')
+
+    db.collection('UserID').find(function (err, result) {
+        if (err) throw err
+
+        console.log(result)
+        
+    })
+
+})
+
